64a42a330b380c715e3ec492fb05e8661dc804f5
Matthew Simo <matthew.a.simo@gmail.com>
2022-06-09 15:14:20 -0500

    tweak og tags

--- src/routes/roster/[uuid]/overview.svelte
+++ src/routes/roster/[uuid]/overview.svelte
@@ -5,11 +5,14 @@ import { prevent_default } from 'svelte/internal';
 	import { fly } from 'svelte/transition';
 
 	export let roster: Roster;
+
+	$: title = `40k tt roster: ${roster.meta.name} - ${roster.meta.catalogueName} ${roster.meta.cost.cp}CP, ${roster.meta.cost.pts}PTS, ${roster.meta.cost.pl}PL`;
+	$: desc = `${roster.forces.length} Force${roster.forces.length > 1 ? 's' : ''}, ${roster.forces.map(force => force.units.length).reduce((pV, cV) => (pV + cV), 0) } Unit${ roster.forces.map(force => force.units.length).reduce((pV, cV) => (pV + cV), 0) > 1 ? 's' : ''}`
 </script>
 
 <svelte:head>
-	<meta property="og:title" content={`40k tt roster: ${roster.meta.name} - ${roster.meta.catalogueName} ${roster.meta.cost.cp} CP ${roster.meta.cost.pts} PTS ${roster.meta.cost.pl} PL`}/>
-	<meta property="og:description" content={`${roster.forces.length} Force${roster.forces.length > 1 && 's'}, ${roster.forces.map(force => force.units.length).reduce((pV, cV) => (pV + cV), 0) } Unit(s)`}/>
+	<meta property="og:title" content={title}/>
+	<meta property="og:description" content={desc}/>
 </svelte:head>
 
 {#if roster}
