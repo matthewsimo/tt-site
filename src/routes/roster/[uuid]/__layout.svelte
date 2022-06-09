a821e2cfdd2207445b115a5daf19a886a1c5b925
Matthew Simo <matthew.a.simo@gmail.com>
2022-06-09 13:57:19 -0500

    move roster index to details & add roster overview page. add roster layout with header & footer

---
+++ src/routes/roster/[uuid]/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import PageFooter from '$lib/components/PageFooter.svelte';
+import PageHeader from '$lib/components/PageHeader.svelte';
+
+	import '../../../app.css';
+</script>
+
+<PageHeader />
+
+<main class="w-full mx-auto px-6 max-w-7xl">
+	<slot />
+</main>
+
+<PageFooter />
