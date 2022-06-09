f8675aaae0f59d528cf5ab4fa8bc0b00e1726600
Matthew Simo <matthew.a.simo@gmail.com>
2022-06-09 15:08:48 -0500

    Add og:title & og:description tabs to roster pages

--- src/routes/roster/[uuid]/details.svelte
+++ src/routes/roster/[uuid]/details.svelte
@@ -6,6 +6,11 @@
 	export let roster: Roster;
 </script>
 
+<svelte:head>
+	<meta property="og:title" content={`40k tt roster: ${roster.meta.name} - ${roster.meta.catalogueName} ${roster.meta.cost.cp} CP ${roster.meta.cost.pts} PTS ${roster.meta.cost.pl} PL`}/>
+	<meta property="og:description" content={`${roster.forces.length} Force${roster.forces.length > 1 && 's'}, ${roster.forces.map(force => force.units.length).reduce((pV, cV) => (pV + cV), 0) } Unit(s)`}/>
+</svelte:head>
+
 {#if roster}
 	<div transition:fly>
 		<RosterDetails {roster} />
--- src/routes/roster/[uuid]/overview.svelte
+++ src/routes/roster/[uuid]/overview.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 	import RosterPreview from '$lib/components/RosterPreview.svelte';
 	import type { Roster } from '$lib/parseRoster';
+import { prevent_default } from 'svelte/internal';
 	import { fly } from 'svelte/transition';
 
 	export let roster: Roster;
 </script>
 
 <svelte:head>
-	<meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
-	<meta property="og:type"               content="article" />
-	<meta property="og:title"              content="When Great Minds Don’t Think Alike" />
-	<meta property="og:description"        content="How much does culture influence creative thinking?" />
+	<meta property="og:title" content={`40k tt roster: ${roster.meta.name} - ${roster.meta.catalogueName} ${roster.meta.cost.cp} CP ${roster.meta.cost.pts} PTS ${roster.meta.cost.pl} PL`}/>
+	<meta property="og:description" content={`${roster.forces.length} Force${roster.forces.length > 1 && 's'}, ${roster.forces.map(force => force.units.length).reduce((pV, cV) => (pV + cV), 0) } Unit(s)`}/>
 </svelte:head>
 
 {#if roster}
